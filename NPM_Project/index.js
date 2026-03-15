080d890186a635ebf4da15aac679f746ef87294b
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-03-15 23:39:11 +0530

    implement basic node structure and experimented with some NPM packages

---
+++ NPM_Project/index.js
@@ -0,0 +1,12 @@
+import generateNames from "sillyname"
+import {randomSuperhero} from 'superheroes';
+
+
+const name = generateNames();
+
+console.log(`My name is ${name}`);
+
+console.log(`My Hero name is ${randomSuperhero()}`);
+
+
+
