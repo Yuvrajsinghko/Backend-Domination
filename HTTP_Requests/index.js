bf2930ad307cbfcb6631d9d98d46c16e84e3c829
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-03-16 23:40:27 +0530

    practiced HTTP request and response functionality

---
+++ HTTP_Requests/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Yuvraj</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>about me</h1>");
+});
+
+app.get("/contact",(req,res)=>{
+    res.send("<p>Contact me for more details.</p>")
+})
