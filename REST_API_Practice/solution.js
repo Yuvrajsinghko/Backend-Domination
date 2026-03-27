b027cf0ad0f09805e0ccfc2855b7fead81a7f83a
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-03-27 22:16:58 +0530

    completed challenge of REST API

--- REST_API_Practice/solution.js
+++ REST_API_Practice/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "f5f71869-0c19-402e-b80f-c92ed915aa8a";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
