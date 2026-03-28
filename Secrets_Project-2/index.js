46462f7e548bb5a3b86d91e5a199deb5a55a90bc
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-03-28 22:19:17 +0530

    completed Secrets project challenge

--- Secrets_Project-2/index.js
+++ Secrets_Project-2/index.js
@@ -1,3 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+const API_URL = "https://secrets-api.appbrewery.com/random";
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL);
+    res.render("index.ejs", {
+      secret: result.data.secret,
+      user: result.data.username,
+    });
+    console.log(result.data);
+  } catch (error) {
+    res.render("index.ejs", { secret: JSON.stringify(error.response.data) });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at port ${port}`);
+});
 // HINTS:
 // 1. Import express and axios
 
